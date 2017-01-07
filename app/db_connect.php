76aa0d8f7132dca205e75851061d2ca472cdd15d
Aditya Sriram <aditya95sriram@gmail.com>
2017-01-07 18:28:22 +0530

    added all backend php files

---
+++ app/db_connect.php
@@ -0,0 +1,28 @@
+<?php
+  
+  /* 
+   * Connects to a MySQL database. Edit parameters in the beginning
+   * to set different connection settings
+   */ 
+
+  $db_host     = 'localhost';
+  $db_user     = 'root';
+  $db_password = '';
+  $db_name     = 'redef_hack';
+
+  $mysqli = new mysqli($db_host, $db_user, $db_password, $db_name);
+  if ($mysqli->connect_errno > 0) {
+    die("Unable to connect to database [{$mysqli->connect_error}]");
+  }
+
+  function insert_input($table, $fields) {
+    $keys = explode(",",$fields);
+    $values = array();
+    foreach ($keys as $key) {
+      $values[] = $_REQUEST[$key];
+    }
+    $values = implode("','", $values);
+    return "INSERT INTO $table ($fields) VALUES ('$values');";
+  }
+
+?>
---
+++ app/fetch_listings.php
@@ -0,0 +1,44 @@
+<?php
+session_start();
+include_once('db_connect.php');
+
+/* example
+fetch_listings.php
+*/
+/*
+$sql = 'SELECT * FROM offers';
+$res = $mysqli->query($sql);
+$res->data_seek(0); // seek result pointer to start
+$offers = array();
+while ($row = $res->fetch_assoc()) {
+  $offers[] = $row;
+}
+$res->close();
+*/
+$sql = 'SELECT * FROM requests WHERE active=1';
+$res = $mysqli->query($sql);
+$res->data_seek(0); // seek result pointer to start
+$requests = array();
+while ($row = $res->fetch_assoc()) {
+  $requests[] = $row;
+}
+$res->close();
+/*
+if (isset($_REQUEST['type'])) {
+  switch($_REQUEST['type']) {
+    case 'offer':
+      $reply = $offers;
+      break;
+    case 'request':
+      $reply = $requests;
+      break;
+    default:
+      $reply = array_merge($requests, $offers);
+  }
+} else {
+  $reply = array_merge($requests, $offers);
+}
+*/
+echo json_encode($requests);
+
+?>

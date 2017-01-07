1369c9d555db7fd9357ec2bb40ce5022eeacad48
0xFALLm <ashimrajkonwar@gmail.com>
2017-01-08 01:09:32 +0530

    xx

--- app/claim_landing.php
+++ app/claim_landing.php
@@ -16,36 +16,58 @@
 </head>
 
 <body>
+
+    <div class='header center-align'>
+        <div class="title">TOTUS</div>
+    </div>
+    <div class="mytoolbar fixed-action-btn toolbar">
+        <a class="btn-floating btn-large red">
+          <i class="large material-icons">menu</i>
+        </a>
+        <ul>
+            <li class="waves-effect waves-light"><a href="#!"><i class="material-icons">insert_chart</i></a></li>
+            <li class="waves-effect waves-light"><a href="#!"><i class="material-icons">format_quote</i></a></li>
+            <li class="waves-effect waves-light"><a href="#!"><i class="material-icons">publish</i></a></li>
+            <li class="waves-effect waves-light"><a href="#!"><i class="material-icons">attach_file</i></a></li>
+        </ul>
+    </div>
+
+    <div class="gap40"></div>
+    <div class="gap40"></div>
   
-  <div class="row">
-    <form class="col s12" method="get" action="send_quotation.php">
-      <input name="request_id" hidden="true"
-      <?php
-        echo "value={$_REQUEST['request_id']}"
-      ?>>
-      <input name="student_id" hidden="true"
-      <?php
-        echo "value={$_REQUEST['student_id']}"
-      ?>>
-      <div class="row">
-        <div class="input-field col s6">
-          <i class="material-icons prefix">play_for_work</i>
-          <input id="points" name="points" type="number" min="0" step="1" class="validate" required>
-          <label for="points" data-error="Points must be a number larger than 0">Points</label>
-        </div>
-      </div>
-      <div class="row">
-        <div class="input-field col s6">
-          <i class="material-icons prefix">trending_up</i>
-          <input id="last_name" name="level" type="number" min="1" step="1" max="5" class="validate" required>
-          <label for="last_name" data-error="Dificulty must be a number between 1 and 5">Difficulty</label>
-        </div>
-      </div>
-      <button class="btn-large waves-effect waves-light" type="submit" name="action">Submit
-        <i class="material-icons right">send</i>
-      </button>
-    </form>
-  </div>  
+
+
+
+    <div class="row claim-section">
+        <form class="col s12" method="get" action="send_quotation.php">
+            <h4 class="center-align tab-header">CLAIM</h4>
+          <input name="request_id" hidden="true"
+          <?php
+            echo "value={$_REQUEST['request_id']}"
+          ?>>
+          <input name="student_id" hidden="true"
+          <?php
+            echo "value={$_REQUEST['student_id']}"
+          ?>>
+          <div class="row">
+            <div class="input-field col s6">
+              <i class="material-icons prefix">play_for_work</i>
+              <input id="points" name="points" type="number" min="0" step="1" class="validate" required>
+              <label for="points" data-error="Points must be a number larger than 0">Points</label>
+            </div>
+          </div>
+          <div class="row">
+            <div class="input-field col s6">
+              <i class="material-icons prefix">trending_up</i>
+              <input id="last_name" name="level" type="number" min="1" step="1" max="5" class="validate" required>
+              <label for="last_name" data-error="Dificulty must be a number between 1 and 5">Difficulty</label>
+            </div>
+          </div>
+          <button class="btn-large waves-effect waves-light" type="submit" name="action">Submit
+            <i class="material-icons right">send</i>
+          </button>
+        </form>
+    </div>  
 	
   <!--JS SCRIPTS  -->
   <script  src="https://code.jquery.com/jquery-2.2.4.min.js"  integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>

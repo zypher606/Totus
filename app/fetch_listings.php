d7da267a4664bd71524d8e9df74d0ec4c25cbffc
Aditya Sriram <aditya95sriram@gmail.com>
2017-01-08 03:42:09 +0530

    add name and rating in fetch listings

--- app/fetch_listings.php
+++ app/fetch_listings.php
@@ -15,7 +15,8 @@ while ($row = $res->fetch_assoc()) {
 }
 $res->close();
 */
-$sql = 'SELECT * FROM requests WHERE active=1';
+//$sql = 'SELECT * FROM requests WHERE active=1';
+$sql = 'SELECT x.id as id,title,student_id,level,lat,lng,time,x.skills as skills,active,description,y.name as name,y.rating as rating FROM requests as x JOIN users as y on x.student_id = y.id WHERE x.active=1';
 $res = $mysqli->query($sql);
 $res->data_seek(0); // seek result pointer to start
 $requests = array();

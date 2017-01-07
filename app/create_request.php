efefc964812873073c3d8cd6a79b56ab78f86940
Aditya Sriram <aditya95sriram@gmail.com>
2017-01-08 03:35:00 +0530

    minor fix

--- app/create_request.php
+++ app/create_request.php
@@ -19,11 +19,35 @@ $values = implode(",", $values);
 $sql = "INSERT INTO requests (poster_id,type,lat,lng,time,skills,active,description) VALUES ($values)";
 */
 $_REQUEST['student_id'] = $_SESSION['user_id'];
-$sql = insert_input('requests', 'student_id,title,lat,lng,time,skills,description');
-echo $sql;
+$sql = insert_input('requests', 'student_id,title,lat,lng,skills,description');
+//echo $sql;
 if ($mysqli->query($sql)) {
-  echo json_encode(['status'=>'success']);
+  $sql = "UPDATE requests SET time=FROM_UNIXTIME({$_REQUEST['time']}) WHERE id={$mysqli->insert_id}";
+  //echo $sql;
+  if ($mysqli->query($sql)) {
+    if (isset($_SESSION['redirect'])) {
+      redirect('../Dashboard');  
+    } else {
+      echo json_encode(['status'=>'success']);
+    }
+  } else {
+    if (isset($_SESSION['redirect'])) {
+      $url = $_SESSION['redirect'];
+      unset($_SESSION['redirect']);
+      $_SESSION['failed']=true;
+      redirect($url);
+    } else {
+      echo json_encode(['status'=>'failed', 'error'=>$mysqli->error]);
+    }
+  }
 } else {
-  echo json_encode(['status'=>'failed', 'error'=>$mysqli->error]);
+    if (isset($_SESSION['redirect'])) {
+      $url = $_SESSION['redirect'];
+      unset($_SESSION['redirect']);
+      $_SESSION['failed']=true;
+      redirect($url);
+    } else {
+      echo json_encode(['status'=>'failed', 'error'=>$mysqli->error]);
+    }
 }
 ?>
